034179e58c69d578bd25a24d0e858af27c12e0df
yliu-fe <34157793+yliu-fe@users.noreply.github.com>
2026-04-08 11:17:32 +0800

    lab01

--- Lab/lab01/lab01.py
+++ Lab/lab01/lab01.py
@@ -8,7 +8,7 @@ def digit(n, k):
     >>> digit(3579, 10)
     0
     """
-    return ____
+    return (n // 10**k) % 10
 
 
 def middle(a, b, c):
@@ -26,7 +26,7 @@ def middle(a, b, c):
     >>> middle(30, 5, 40)
     30
     """
-    return ____
+    return a + b + c - min(a, b, c) - max(a, b, c)
 
 
 def falling(n, k):
@@ -41,7 +41,10 @@ def falling(n, k):
     >>> falling(4, 0)
     1
     """
-    "*** YOUR CODE HERE ***"
+    if k == 0:
+        return 1
+    else:
+        return n * falling(n - 1, k - 1)
 
 
 def divisible_by_k(n, k):
@@ -64,7 +67,12 @@ def divisible_by_k(n, k):
     >>> c
     0
     """
-    "*** YOUR CODE HERE ***"
+    count = 0
+    for i in range(1, n + 1):
+        if i % k == 0:
+            print(i)
+            count += 1
+    return count
 
 
 def sum_digits(y):
@@ -81,6 +89,12 @@ def sum_digits(y):
     6
     """
     "*** YOUR CODE HERE ***"
+    sum = 0
+    while y > 0:
+        sum += y % 10
+        y //= 10
+    return sum
+
 
 
 def double_eights(n):
@@ -99,4 +113,8 @@ def double_eights(n):
     False
     """
     "*** YOUR CODE HERE ***"
-
+    if n < 10:
+        return False
+    if n % 100 == 88:
+        return True
+    return double_eights(n // 10)
